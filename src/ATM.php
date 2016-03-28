0dcc3d1a7300c5192702deca23b5238eb7326c06
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-03-28 17:42:11 +0100

    Get sessions data from input

--- src/ATM.php
+++ src/ATM.php
@@ -2,6 +2,11 @@
 
 class ATM
 {
+    const BALANCE_INQUIRY_SYMBOL = 'B';
+    const BALANCE_INQUIRY = 1;
+    const WITHDRAWAL_SYMBOL = 'W';
+    const WITHDRAWAL = 2;
+
     private $totalCash;
     private $sessions = array();
 
@@ -20,7 +25,73 @@ class ATM
         }
     }
 
-    private function extractSessions($sessions)
+    private function extractSessions($input)
     {
+        $sessions = explode("\n\n", $input);
+        foreach ($sessions as $session) {
+            $lines = explode("\n", $session, 3);
+            if (count($lines) < 3) {
+                throw new \InvalidArgumentException('Wrong argument. Each session consists of at least 3 lines');
+            }
+
+            $this->sessions[] = array_merge(
+                $this->extractAccountNumberAndPINs($lines[0]),
+                $this->extractBalanceAndOverdraft($lines[1]),
+                $this->extractTransactions($lines[2])
+            );
+        }
+    }
+
+    private function extractAccountNumberAndPINs($line)
+    {
+        $words = explode(" ", $line);
+        if (count($words) != 3 || !is_numeric($words[0]) || !is_numeric($words[1]) || !is_numeric($words[2])) {
+            throw new \InvalidArgumentException("Wrong argument. The first line of a session should contain the user's account number, correct PIN and the PIN they actually entered.");
+        }
+
+        return array(
+          'accountNumber' => $words[0],
+          'correctPIN'    => $words[1],
+          'introducedPIN' => $words[2],
+        );
+    }
+
+    private function extractBalanceAndOverdraft($line)
+    {
+        $words = explode(" ", $line);
+        if (count($words) != 2 || !is_numeric($words[0]) || !is_numeric($words[1])) {
+            throw new \InvalidArgumentException("Wrong argument. The second line of a session should contain the user's balance and overdraft facility.");
+        }
+        return array(
+          'balance'   => $words[0],
+          'overdraft' => $words[1],
+        );
+    }
+
+    private function extractTransactions($input)
+    {
+        $transactions = array();
+
+        $lines = explode("\n", $input);
+        foreach ($lines as $line) {
+            $words = explode(" ", $line);
+
+            if (count($words) == 1 && $words[0] == self::BALANCE_INQUIRY_SYMBOL) {
+                $transactions[] = array(
+                  'type' => self::BALANCE_INQUIRY
+                );
+            } elseif (count($words) == 2 && $words[0] == self::WITHDRAWAL_SYMBOL && is_numeric($words[1])) {
+                $transactions[] = array(
+                  'type' => self::WITHDRAWAL,
+                  'amount' => $words[1],
+                );
+            } else {
+                throw new \InvalidArgumentException("Wrong argument. Invalid transaction");
+            }
+        }
+
+        return array(
+          'transactions' => $transactions
+        );
     }
 }
--- tests/ATMTest.php
+++ tests/ATMTest.php
@@ -4,7 +4,7 @@ class ATMTest extends \PHPUnit_Framework_TestCase
 {
     public function testInputWithNoSecondBlankLine()
     {
-        $input = $text = <<<'EOT'
+        $input = <<<'EOT'
 8000
 text
 EOT;
@@ -14,9 +14,93 @@ EOT;
 
     public function testInputWithInvalidTotalCash()
     {
-        $input = $text = <<<'EOT'
+        $input = <<<'EOT'
 text
 
+EOT;
+        $this->setExpectedException('InvalidArgumentException');
+        $atm = new ATM($input);
+    }
+
+    public function testInputWithInvalidSessionFirstLine_Number()
+    {
+        $input = <<<'EOT'
+8000
+
+123 123 123 123
+EOT;
+        $this->setExpectedException('InvalidArgumentException');
+        $atm = new ATM($input);
+    }
+
+    public function testInputWithInvalidSessionFirstLine_Type()
+    {
+        $input = <<<'EOT'
+8000
+
+aaa bbb ccc
+EOT;
+        $this->setExpectedException('InvalidArgumentException');
+        $atm = new ATM($input);
+    }
+
+    public function testInputWithInvalidSessionSecondLine_Number()
+    {
+        $input = <<<'EOT'
+8000
+
+12345678 1234 1234
+aaa
+EOT;
+        $this->setExpectedException('InvalidArgumentException');
+        $atm = new ATM($input);
+    }
+
+    public function testInputWithInvalidSessionSecondLine_Type()
+    {
+        $input = <<<'EOT'
+8000
+
+12345678 1234 1234
+500 aaa
+EOT;
+        $this->setExpectedException('InvalidArgumentException');
+        $atm = new ATM($input);
+    }
+
+    public function testInputWithInvalidSessionNoThirdLine()
+    {
+        $input = <<<'EOT'
+8000
+
+12345678 1234 1234
+500 100
+EOT;
+        $this->setExpectedException('InvalidArgumentException');
+        $atm = new ATM($input);
+    }
+
+    public function testInputWithInvalidTransactionType()
+    {
+        $input = <<<'EOT'
+8000
+
+12345678 1234 1234
+500 100
+C
+EOT;
+        $this->setExpectedException('InvalidArgumentException');
+        $atm = new ATM($input);
+    }
+
+    public function testInputWithInvalidTransactionAmount()
+    {
+        $input = <<<'EOT'
+8000
+
+12345678 1234 1234
+500 100
+W aaa
 EOT;
         $this->setExpectedException('InvalidArgumentException');
         $atm = new ATM($input);
