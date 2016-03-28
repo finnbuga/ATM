a38e65d5f82550ed014b8c4cba53d482dbd08e1a
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2016-03-28 14:48:42 +0100

    Add the ATM and the ATMTest classes

---
+++ src/ATM.php
@@ -0,0 +1,8 @@
+<?php
+
+class ATM
+{
+    public function __construct()
+    {
+    }
+}
---
+++ tests/ATMTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class ATMTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $atm = new ATM();
+        $this->assertTrue(true);
+    }
+}
